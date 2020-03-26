1da3ffa0e27b5109cd6fbb756c245f203d0dc965
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-27 08:16:11 +0900

    Spring and IOC

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -1,38 +1,19 @@
 package kr.co.fastcampus.cli;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.sql.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 class Main {
 	private static Logger logger = LoggerFactory.getLogger(Main.class);
 	
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) {
 		logger.info("Hello, world!!");
 
-		Class.forName("org.h2.Driver");
-		var url = "jdbc:h2:mem:test;MODE=MySQL;";
+		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
 
-		try(var connection = DriverManager.getConnection(url, "sa", "");
-			var statement = connection.createStatement()) {
-
-			connection.setAutoCommit(false);
-			statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-			try {
-				statement.executeUpdate("insert into member(username, password) values ('flyingmt', '1234')");
-				connection.commit();
-			} catch(SQLException e) {
-				connection.rollback();
-			}
-
-			var resultSet = statement.executeQuery("select id, username, password from member");
-			while(resultSet.next()) {
-				var member = new Member(resultSet);
-				logger.info(member.toString());
-			}
-		} catch (SQLException e) {
-
-		}
-
+		var dao = context.getBean("dao", Dao.class);
+		dao.run();
 	}
 }

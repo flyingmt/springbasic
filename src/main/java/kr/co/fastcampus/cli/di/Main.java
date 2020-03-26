6f1c3a7edda48ff3be4ce2b81e074e031a6c86f8
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-27 08:35:47 +0900

    Ioc

--- src/main/java/kr/co/fastcampus/cli/di/Main.java
+++ src/main/java/kr/co/fastcampus/cli/di/Main.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.cli.di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        boolean condition = true;
-        B b = new B(condition);
-        A a = new A(b);
+        ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+        A a = context.getBean("a", A.class);
         a.print();
     }
 }

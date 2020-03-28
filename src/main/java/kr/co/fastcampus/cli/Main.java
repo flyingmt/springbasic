d2468d3a0bb4c6b95c759d6b5311909921ddb8f7
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-28 10:48:19 +0900

    Bean scope

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -1,5 +1,6 @@
 package kr.co.fastcampus.cli;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -7,11 +8,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 
+@Slf4j
 class Main {
-	private static Logger logger = LoggerFactory.getLogger(Main.class);
-	
 	public static void main(String[] args) throws SQLException {
-		logger.info("Hello, world!!");
+		log.info("Hello, world!!");
 
 		try {
 			Class.forName("org.h2.Driver");
@@ -19,9 +19,15 @@ class Main {
 			e.printStackTrace();
 		}
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 
-		var dao2 = context.getBean("dao2", Dao2.class);
-		dao2.run();
+		A a1 = context.getBean("A", A.class);
+		A a2 = context.getBean("A", A.class);
+
+		log.info("result: " + (a1 == a2));
+
+		//var dao2 = context.getBean("dao2", Dao2.class);
+		//dao2.run();
 	}
 }

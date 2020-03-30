b8a015e9a9b4329375800a4db52f7136a93f7c96
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-31 07:57:56 +0900

    lifecyle 코드 추가.

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.Lifecycle;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.Connection;
@@ -16,9 +18,16 @@ class Main {
 		log.info("Hello, world!!");
 
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-		ConnectionFactory factory = context.getBean(ConnectionFactory.class);
-		Connection connection = factory.getConnection();
-		log.info("" + (connection != null));
+		val lifecyle = context.getBean(Lifecycle.class);
+
+		log.info(">> 1 : " + lifecyle.isRunning());
+
+		//ConnectionFactory factory = context.getBean(ConnectionFactory.class);
+		//Connection connection = factory.getConnection();
+		//log.info("" + (connection != null));
 		context.close();
+
+		log.info(">> 2 : " + lifecyle.isRunning());
+
 	}
 }

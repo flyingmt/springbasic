6c060c274fce2319084f4b9f50ed41c8eb3fbd6c
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-31 08:25:20 +0900

    ApplicationContext Autowired

--- src/main/java/kr/co/fastcampus/cli/A.java
+++ src/main/java/kr/co/fastcampus/cli/A.java
@@ -2,15 +2,17 @@ package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 import javax.annotation.PostConstruct;
 
 @Slf4j
 public class A {
     @Autowired private B b;
+    @Autowired private ApplicationContext context;
 
     @PostConstruct
     void init() {
-        log.info("" + b);
+        log.info("" + context);
     }
 }

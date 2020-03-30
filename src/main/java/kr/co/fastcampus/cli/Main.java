3f67f0d5665fe59d0de3e1e5c6af235b711ad9b6
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-31 08:18:15 +0900

    constructor autowired

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -1,15 +1,8 @@
 package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.Lifecycle;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
@@ -18,16 +11,11 @@ class Main {
 		log.info("Hello, world!!");
 
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-		val lifecyle = context.getBean(Lifecycle.class);
-
-		log.info(">> 1 : " + lifecyle.isRunning());
 
 		//ConnectionFactory factory = context.getBean(ConnectionFactory.class);
 		//Connection connection = factory.getConnection();
 		//log.info("" + (connection != null));
 		context.close();
-
-		log.info(">> 2 : " + lifecyle.isRunning());
 
 	}
 }

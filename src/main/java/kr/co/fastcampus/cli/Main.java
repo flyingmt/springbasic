d7f3c7288e69e21d145845b147dff46ff14feab1
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-31 11:01:35 +0900

    profiles

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -2,6 +2,7 @@ package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
@@ -11,8 +12,9 @@ class Main {
 	public static void main(String[] args) throws SQLException {
 		log.info("Hello, world!!");
 
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(AppDefaultConfig.class, AppDevConfig.class);
+		context.refresh();
 		var dao = context.getBean(Dao.class);
 		dao.run();
 

668377a5da907c16f7ff6e077af543ea7e7d4e3e
Jun-Hee Lee <flyingmt@gmail.com>
2020-03-31 10:27:56 +0900

    context:property-placeholder

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -1,11 +1,9 @@
 package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.sql.SQLException;
 
 @Slf4j
@@ -13,13 +11,10 @@ class Main {
 	public static void main(String[] args) throws SQLException {
 		log.info("Hello, world!!");
 
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		//context.register(AppConfig.class);
-		context.scan("kr.co.fastcampus.cli");
-		context.refresh();
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
 
-		val b = context.getBean(B.class);
-		log.info("" + b);
+		var dao = context.getBean(Dao.class);
+		dao.run();
 
 		context.close();
 
